3b283ffc95a9b6e69d1a3310c813e464632d3e8b
Chazni Katz <ckatz@astutefse.com>
2020-04-19 03:59:13 +0200

    added classes for equation and polynomial

--- Parser.py
+++ Parser.py
@@ -1,26 +1,48 @@
 import sys
 
+class Polynomial:
+    def __init__(self, sign, coefficient, variable, degree):
+        self.sign = sign
+        self.coefficient = coefficient
+        self.variable = variable
+        self.degree = degree
+
+
+class Equation:
+    def __init__(self):
+        self.list_of_polynomials = []
+
+
 class Parser:
-    def __init__(self, expression):
-        self.expression = expression
+    def __init__(self):
 
         if len(sys.argv) != 2:
             raise Exception("There should only be 2 arguments")
 
     def remove_white_space(self, expression):
-        expression = ""
-        for letter in expression:
-            if letter == " ":
+        expression_no_spaces = ""
+        for char in expression:
+            if char == " ":
                 continue
             else:
-                expression += letter
-        return expression
+                expression_no_spaces += char
+        return expression_no_spaces
+
+    def separate_into_parts(self, expression):
+        parts = []
+        for char in expression:
 
 
-    
+       
 
 
 
-p = Parser("")
-exp = p.remove_white_space("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
-p.separate_into_parts(exp)
+
+            
+
+
+
+
+parser = Parser()
+expression = parser.remove_white_space(sys.argv[1])
+parser.separate_into_parts(expression)
