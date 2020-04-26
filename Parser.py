c6615dffc31e848e5901f95e0d38789b5cb99f8f
Chazni Katz <ckatz@astutefse.com>
2020-04-26 05:46:39 +0200

    Extract tokens and print simplified expression

--- Parser.py
+++ Parser.py
@@ -1,48 +1,52 @@
-import sys
+import sys, re
 
-class Polynomial:
-    def __init__(self, sign, coefficient, variable, degree):
-        self.sign = sign
-        self.coefficient = coefficient
-        self.variable = variable
-        self.degree = degree
-
-
-class Equation:
+class Lexer:
     def __init__(self):
-        self.list_of_polynomials = []
-
-
-class Parser:
-    def __init__(self):
+        self.lhs = []
+        self.rhs = []
 
         if len(sys.argv) != 2:
             raise Exception("There should only be 2 arguments")
 
-    def remove_white_space(self, expression):
-        expression_no_spaces = ""
-        for char in expression:
-            if char == " ":
-                continue
-            else:
-                expression_no_spaces += char
-        return expression_no_spaces
+    def split_into_lhs_rhs(self, expression):
+        expression_no_space = expression.replace(" ", "")
+        lhs_rhs = expression_no_space.split("=")
+        return lhs_rhs
 
-    def separate_into_parts(self, expression):
-        parts = []
-        for char in expression:
+    def tokenize(self, expression):
+        try:
+            lhs = self.split_into_lhs_rhs(expression)[0]
+            rhs = self.split_into_lhs_rhs(expression)[1]
+            self.lhs = re.findall(r'[-+]?\d*\.?\d+\*[a-zA-Z]\^\d*', lhs)
+            self.rhs = re.findall(r'[-+]?\d*\.?\d+\*[a-zA-Z]\^\d*', rhs)
+        except:
+            print("Invalid expression, can not be split into left and right hand sides")
+        
+
+class Parser:
+
+    def simplify_expression(self):
+        lexer = Lexer()
+        lexer.tokenize(sys.argv[1])
+        new_token = ""
+        for token in lexer.rhs:
+            if token[0] == "+":
+                new_token = "-" + token[1:]
+            elif str.isdigit(token[0]):
+                new_token += "-" + token
+            elif token[0] == "-":
+                new_token = "+" + token[1:]
+            lexer.lhs.append(new_token)
+        return lexer.lhs
+
+    def print_simplified_expression(self):
+        expression_list = self.simplify_expression()
+        expression = "Reduced form: "
+        for token in expression_list:
+            expression += token
+        expression += " = 0"
+        print(expression, end="")
 
 
-       
-
-
-
-
-            
-
-
-
-
-parser = Parser()
-expression = parser.remove_white_space(sys.argv[1])
-parser.separate_into_parts(expression)
+p = Parser()
+p.print_simplified_expression()
